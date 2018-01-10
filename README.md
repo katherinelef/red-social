84ad8410d9efaeb424fe784440328c7435cbc879
Lauraisa <galindolaura.isabel@gmail.com>
2018-01-10 11:18:55 -0500

    trabajando readme, opacity en eventos

---
+++ README.md
@@ -0,0 +1,6 @@
+# Green Food ![imagen](docs/logo.png) 
+
+
+![imagen](docs/plato.jpg)
+
+### Muchas veces salimos de casa con los amigos o familia para disfrutar una agradable reunión, pero 
--- js/aliment.js
+++ js/aliment.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
   var $picture = $('#aliment');
   function show(event) {
     for (i = 0; i < data.length; i++) {
-      $picture.append('<div class="col-xs-12 col-sm-4"><div class="card-grid "><div class="front "><img class="img-responsive" src=' + data[i].img + '></div>' + '<div class="back"><p>' + data[i].description + '</p></div></div></div></div>');
+      $picture.append('<div class="col-xs-12 col-sm-4"><div class="card-grid "><div class="front "><img class="img-responsive" src=' + data[i].img + '></div>' + '<div class="back"><p>' + data[i].description + '</p></div></div></div>');
     }
   }
   show();
   $('.card-grid').flip({trigger:'hover'});
 
-  // evento para eventos.html
-  $('.container-fluid img').each(function(contador) {
+  // // evento para eventos.html
+  $('.container-fluid .flip-text ').each(function(contador) {
     $(this).delay((contador)*500).fadeTo(1000,1);
   })
 });
