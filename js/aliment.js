1eb35e78d4df6d8d7158a1a1932486cdc29a46d0
Lauraisa <galindolaura.isabel@gmail.com>
2018-01-10 01:58:07 -0500

    añadiendo html de alim y funcionlidad

---
+++ js/aliment.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  var $picture = $('#aliment');
+  function show(event) {
+    for (i = 0; i < data.length; i++) {
+      $picture.append('<div class="col-xs-12 col-sm-4"><div class="card-grid "><div class="front "><img class="img-responsive" src=' + data[i].img + '></div>' + '<div class="back"><p>' + data[i].description + '</p></div></div></div></div>');
+    }
+  }
+  show();
+  $('.card-grid').flip({trigger:'hover'});
+});
---
+++ js/data.js
@@ -0,0 +1,53 @@
+var data = [
+  {
+    'name':'Semillas',
+    'img':'../assets/images/semillas.jpg',
+    'description': 'Las semillas aportan deliciosos toques de sabor a nuestras recetas.',
+  },
+  {
+    'name':'Legumbres',
+    'img':'../assets/images/legumbres.jpg',
+    'description': 'Las legumbres proporcionan proteínas vegetales de calidad y son un alimento básico y barato.',
+  },
+  {
+    'name':'Verduras',
+    'img':'../assets/images/verduras.jpg',
+    'description':'La variedad de verduras y hortalizas es casi infinita. Podemos ahorrar dinero si compramos verduras de temporada.'
+  },
+  {
+    'name':'Hiervas y especias',
+    'img':'../assets/images/hierbas_especias.jpg',
+    'description': 'Son los ingredientes saborizantes por excelencia en todas las cocinas del mundo.',
+  },
+  {
+    'name':'Cereales',
+    'img':'../assets/images/cereales.jpg',
+    'description': 'Son uno de los alimentos básicos para millones de personas en el mundo.',
+  },
+  {
+    'name':'Frutas',
+    'img':'../assets/images/frutas.jpg',
+    'description': 'Son una excelente fuente de vitaminas, minerales y fibra.',
+  },
+  {
+    'name':'Harinas',
+    'img':'../assets/images/harinas.jpg',
+    'description': 'Sirven tanto para rebozar, como para aglutinar, hacer pasta y noodles, bollería y repostería, etc.',
+  },
+  {
+    
+    'name':'Frutos secos',
+    'img':'../assets/images/frutos_secos.jpg',
+    'description': 'Aportan vitaminas, minerales y ácidos grasos omega-3, entre otros nutrientes.',
+  },
+  {
+    'name':'Aceites',
+    'img':'../assets/images/aceites.jpg',
+    'description': 'Los aceites y grasas sólidas (de coco, manteca de cacao, margarinas vegetales) son básicas y las utilizamos a diario.',
+  },
+  {
+    'name':'Salsas',
+    'img':'../assets/images/salsas.jpg',
+    'description':'Tener vinagres y salsas en la cocina siempre nos viene bien para conseguir interesante sabores en nuestros platos.',
+  },
+]
